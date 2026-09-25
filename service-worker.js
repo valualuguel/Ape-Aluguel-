317689b101552f60adf07c7a9cd5b849828db5c6
valualuguel <valu.aluguel@gmail.com>
2026-09-25 12:13:25 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='aluguéis-v2';
+const CACHE='aluguéis-v3';
 const FILES=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
